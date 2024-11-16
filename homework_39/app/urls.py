9b7633db72bd50f95c7130f1aef5bb781788e2de
otari <xondanoza@gmail.com>
2024-11-16 08:57:19 -0800

    lecture 41

--- homework_39/app/urls.py
+++ homework_39/app/urls.py
@@ -5,4 +5,5 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('book/add/', views.add_book, name='add_book' ),
+    path('book/detail/<int:pk>/', views.book_details, name='book_details')
 ]
--- homework_39/app/views.py
+++ homework_39/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Category, Book
 from .forms import BookForm
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 def index(request):
     filter_book = request.GET.get('search')
     filter_category = request.GET.getlist('category')
@@ -14,6 +14,17 @@ def index(request):
         books = Book.objects.all()
 
     categories = Category.objects.all()
+
+    paginator = Paginator(books, 6)
+
+    try:
+        page_num = request.GET.get('page')
+        books = paginator.page(page_num)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+
     return render(request, 'index.html', {'books': books, 'categories': categories})
 
 
@@ -27,3 +38,8 @@ def add_book(request):
     else:
         form = BookForm()
         return render(request, 'add_book.html', {'form': form})
+    
+def book_details(request, pk):
+    book = Book.objects.get(pk=pk)
+    related_books = Book.objects.filter(categories__in=book.categories.all()).exclude(pk=pk)
+    return render(request, 'book_details.html', {'book': book, 'related_books': related_books})
